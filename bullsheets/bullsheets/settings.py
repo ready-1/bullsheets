cf752e7dbd7e962f3ad23d9329fe9c5526005a9f
Bob King <bob@getready1.com>
2024-09-28 20:32:30 -0700

    add crispy-bootstrap5

--- bullsheets/bullsheets/settings.py
+++ bullsheets/bullsheets/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'crispy_forms',
+    'crispy_bootstrap5',
     'home',
     'vets',
     "django.contrib.admin",
@@ -124,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
